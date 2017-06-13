98b642ee942737802c4ceb64cd933b871a5a4484
Lars Nolden <larsnolden@web.de>
2017-06-13 19:59:44 -0400

    build deploy version

--- src/configureStore.js
+++ src/configureStore.js
@@ -10,16 +10,14 @@ import io from 'socket.io-client';
 
 import rootReducer from './rootReducer';
 
-export let socket = io('http://localhost:3003');
+export let socket = io('http://larsnolden.com:3003');
 let socketIoMiddleware = createSocketIoMiddleware(socket, "server/");
 
 let middleware = [socketIoMiddleware, thunk];
 
 export default function configureStore(preloadedState) {
-  console.log(preloadedState)
   if (preloadedState) preloadedState = { list: { ...preloadedState } }
   else preloadedState = { list: { todos: [] } }
-  console.log(`cached state: ${JSON.stringify(preloadedState)}`)
   return createStore(
     rootReducer,
     preloadedState,
